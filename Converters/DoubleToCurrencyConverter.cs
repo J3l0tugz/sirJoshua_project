31ebc51b4ae7c3cd46f627db9ff6ded72a80d039
Kaisodesu <alrickjelotugano@gmail.com>
2024-12-08 02:26:58 +0800

    Added History View template

---
+++ Converters/DoubleToCurrencyConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Mamilots_POS.Converters
+{
+    public class DoubleToCurrencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return $"₱{doubleValue:F2}";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ViewModels/HistoryPageViewModel.cs
+++ ViewModels/HistoryPageViewModel.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using Avalonia.Media.Imaging;
+using Mamilots_POS.Helpers;
+using Mamilots_POS.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mamilots_POS.ViewModels
 {
-    internal class HistoryPageViewModel : ViewModelBase
-    {
+    
+    
+        internal partial class HistoryPageViewModel : ViewModelBase, INotifyPropertyChanged
+        {
+            private ObservableCollection<transaction> _transactions;
+
+            public ObservableCollection<transaction> transactions
+            {
+                get => _transactions;
+                set
+                {
+                    if (_transactions != value)
+                    {
+                        _transactions = value;
+                        OnProductPropertyChanged();
+                    }
+                }
+            }
+
+            public HistoryPageViewModel(AuthenticationResult authResult)
+            {
+
+            }
+
+            // Constructor to set the transactionNames
+            public HistoryPageViewModel()
+            {
+                transactions = new ObservableCollection<transaction>
+            {
+            new transaction { OrderNumber = "#01", TotalPrice = 120.00},
+            new transaction { OrderNumber = "#02", TotalPrice = 5000.00}
+
+                };
+            }
+
+            public event PropertyChangedEventHandler ProductPropertyChanged;
+
+            protected void OnProductPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                ProductPropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+
+        }
+
+        public class transaction
+        {
+
+            public string? OrderNumber { get; set; }
+            public Double? TotalPrice { get; set; }
+
     }
+
 }
