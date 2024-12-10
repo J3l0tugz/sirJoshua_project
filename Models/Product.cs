02f91f86dcd50b890e56b7683e60fcafe9f560cc
camachoRamel <142206766+camachoRamel@users.noreply.github.com>
2024-12-10 09:58:59 +0800

    Fixed assets path

---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mamilots_POS.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public float Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; private set; }
+    }
+}
